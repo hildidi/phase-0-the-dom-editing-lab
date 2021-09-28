850fdcc17effea3afb4f52a70e370134d3e04095
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-09-27 22:47:22 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+const main = document.getElementById("main");
+main.innerHTML =
+  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
